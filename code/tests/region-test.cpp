da7a1bd7c353df94ac8a54fff6338edec67a9261
Toke Høiland-Jørgensen <toke@toke.dk>
2012-03-29 15:48:19 +1300

    Add more region tests.

--- code/tests/region-test.cpp
+++ code/tests/region-test.cpp
@@ -15,10 +15,22 @@ int main(int argc, char *argv[])
 {
   Region one, two;
 
+  printf("Adding point (0,0)\n");
   one.add(RPoint(0,0));
-  one.add(RPoint(0,1));
-  one.add(RPoint(1,0));
-  one.add(RPoint(1,1));
-
   one.print();
+  printf("Adding point (0,1)\n");
+  one.add(RPoint(0,1));
+  one.print();
+  printf("Inboundary(0,0): %s\n", one.inBoundary(RPoint(0,0)) ? "true" : "false");
+  printf("contains(1,0): %s\n", one.contains(RPoint(1,0)) ? "true" : "false");
+  printf("adjacentPoint(1,0): %s\n", one.adjacentPoint(RPoint(1,0)) ? "true" : "false");
+  printf("inBoundary(1,0): %s\n", one.inBoundary(RPoint(1,0)) ? "true" : "false");
+  printf("interior(1,0): %s\n", one.interior(RPoint(1,0)) ? "true" : "false");
+  printf("Adding point (1,0)\n");
+  one.add(RPoint(1,0));
+  one.print();
+  printf("Adding point (1,1)\n");
+  one.add(RPoint(1,1));
+  one.print();
+
 }
