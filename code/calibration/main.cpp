c234ff2bdb552ecc5a60d752044d274e3dd8228b
Ben Meadows <bmea011@aucklanduni.ac.nz>
2012-04-04 12:56:10 +1200

    Calibrator now reads in image width and height from same file as list of calibration points

--- code/calibration/main.cpp
+++ code/calibration/main.cpp
@@ -5,11 +5,13 @@
 
 // USAGE: "./CamCalibrator.exe <image_points_input_file.txt>"
 // Defaults to using "test.txt", which contains points from ImageJ on img160027
+// The input file must begin with the length (x) and height (y) of the image in pixels.
 int main(int argc, char *argv[])
 {
 	CamCalibrator cc(argc, argv);
 	cc.getPtsFromSegmentedImage();	// Stage 1: get image coordinates of calibration points
 	cc.matchPtsToCalibrationPts();	// Stage 2: match image calibration points to measured points
 	cc.calibrate();					// Stage 3: do the camera calibration
+	cc.checkResults();				// Stage 4: back-project rays using the calculated information
 	return 0;
 }
