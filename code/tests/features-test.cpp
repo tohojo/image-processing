5c142d908fbcedfbb526ac46958cefa5f27f9295
Toke Høiland-Jørgensen <toke@toke.dk>
2012-04-10 21:18:30 +1200

    Test program for feature point extraction.

---
+++ code/tests/features-test.cpp
@@ -0,0 +1,73 @@
+/**
+ * features-test.cpp
+ *
+ * Toke Høiland-Jørgensen
+ * 2012-04-10
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cv.h>
+#include <highgui.h>
+#include "fast_hessian.h"
+
+using namespace std;
+using namespace cv;
+
+float dist(Point a, Point b)
+{
+  return sqrt(pow(a.x+b.x,2)+pow(a.y+b.y,2));
+}
+
+int main(int argc, char** argv)
+{
+  if(argc < 3) {
+    cerr << "Usage: " << argv[0] << " <imagefile> <pointsfile>" << endl;
+    return 1;
+  }
+  Mat img = imread(argv[1], 0);
+
+  string line;
+  ifstream in(argv[2], ios::in);
+  list<Point> points;
+
+  int in_x, in_y;
+  while(in) {
+    if(getline(in, line)) {
+      stringstream(line) >> in_x >> in_y;
+      points.push_back(Point(in_x, in_y));
+    }
+  }
+
+  cerr << img.cols << "x" << img.rows << " " << points.size() << endl;
+  for(list<Point>::iterator i = points.begin(); i != points.end(); ++i) {
+    cerr << "(" << i->x << "," << i->y << ")" << endl;
+  }
+
+  float threshold = 1;
+  float step = 5;
+  float max = 600;
+  float dist_fuzz = 2;
+  list<KeyPoint> kpts;
+
+  int matches;
+
+  cout << "threshold,matches,false-neg,false-pos" << endl;
+
+
+  for(;threshold <= max; threshold += step) {
+    matches = 0;
+    FastHessian fh(img, 4, 4, 2, threshold);
+    fh.compute();
+    kpts = fh.interestPoints().toStdList();
+    for(list<KeyPoint>::iterator i = kpts.begin(); i != kpts.end(); ++i) {
+      for(list<Point>::iterator j = points.begin(); j != points.end(); ++j) {
+        if(dist(i->pt, *j) <= dist_fuzz) matches++;
+      }
+    }
+    cout << threshold << "," << matches << "," << points.size()-matches << "," << kpts.size()-matches << endl;
+  }
+
+}
+
