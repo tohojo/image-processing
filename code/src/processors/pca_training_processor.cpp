1f5acb75c63d15e37f6d5d6ddadb9900c762aff7
Ben Meadows <bmea011@aucklanduni.ac.nz>
2012-05-28 16:00:43 +1200

    Tweaked some PCA

--- code/src/processors/pca_training_processor.cpp
+++ code/src/processors/pca_training_processor.cpp
@@ -64,7 +64,7 @@ bool PcaTrainingProcessor::loadImages(){
 
 	cout << "numImages: " << numImages << "\n";
 	cout << "pixelsPerImage: " << pixelsPerImage << "\n";
-	trainingSetImages = Mat(pixelsPerImage, numImages, CV_32F); // Use floats so we can subtract averages etc
+	trainingSetImages = Mat(pixelsPerImage, numImages, CV_64FC1); // Use double so we can subtract averages etc
 	// Remainder of file is image names
 	// **MUST** HAVE A TRAILING NEWLINE!
 	int counter = 0;
@@ -73,10 +73,10 @@ bool PcaTrainingProcessor::loadImages(){
 		string str = qstr.toStdString();
 		str = str.erase(str.length()-1);
 		cout << "Reading: " << str << "\n";
-		Mat img = imread(str, 0); //greyscale
+		Mat img = imread(str, 0); // not in colour yet
 		img = img.reshape(img.channels(), img.rows*img.cols);
 		for (int i = 0; i < pixelsPerImage; i++){
-			trainingSetImages.at<float>(i,counter) = (0.0 + img.at<unsigned char>(i,0));
+			trainingSetImages.at<double>(i,counter) = (0.0 + img.at<unsigned char>(i,0));
 		}
 		counter++;
 	}
@@ -84,7 +84,7 @@ bool PcaTrainingProcessor::loadImages(){
 	cout << "Training set cols: " << trainingSetImages.cols << "\n";
 	for (int i = 0; i < trainingSetImages.rows; i++){
 		for (int j = 0; j < trainingSetImages.cols; j++){
-			cout << " " << trainingSetImages.at<float>(i,j);
+			cout << " " << trainingSetImages.at<double>(i,j);
 		}
 		cout << "\n";
 	}
@@ -101,68 +101,100 @@ bool PcaTrainingProcessor::PCATrain(){
 
 	// Centre dataset with averages
 	for (int i = 0; i < trainingSetImages.rows; i++){
-		float average = 0;
+		double average = 0;
 		for (int j = 0; j < trainingSetImages.cols; j++){
-			average += trainingSetImages.at<float>(i,j);
+			average += trainingSetImages.at<double>(i,j);
 		}
 		average /= trainingSetImages.cols;
 		for (int j = 0; j < trainingSetImages.cols; j++){
-			trainingSetImages.at<float>(i,j) -= average;
+			trainingSetImages.at<double>(i,j) -= average;
 		}
 	}
 
-	cout << "\n";
+	cout << "\nCentred data:\n";
 	for (int i = 0; i < trainingSetImages.rows; i++){
 		for (int j = 0; j < trainingSetImages.cols; j++){
-			cout << " " << trainingSetImages.at<float>(i,j);
+			cout << " " << trainingSetImages.at<double>(i,j);
 		}
 		cout << "\n";
 	}
 
 	// 2. Compute covariance matrix of centred dataset.
 	// C = sum[1...N] of yi * yi(transpose)
-	Mat covarianceMat = Mat(pixelsPerImage, pixelsPerImage, CV_32F);
+	Mat covarianceMat = Mat(pixelsPerImage, pixelsPerImage, CV_64FC1, Scalar(0));
 	for (int i = 0; i < numImages; i++){
-		covarianceMat = covarianceMat + (trainingSetImages.col(i) * (trainingSetImages.col(i).t()) );
+		Mat matToAdd = trainingSetImages * trainingSetImages.t();
+		covarianceMat = covarianceMat + matToAdd;
 	}
 
-	cout << "\nCovariance\n";
+	cout << "\nCovariance matrix:\n";
 	for (int i = 0; i < covarianceMat.rows; i++){
 		for (int j = 0; j < covarianceMat.cols; j++){
-			cout << " " << covarianceMat.at<float>(i,j);
+			cout << " " << covarianceMat.at<double>(i,j);
 		}
 		cout << "\n";
 	}
 
+
+	// THIS IS A TEST TO SEE WHETHER THE 'eigen' FUNCTION WORKS
+	double b[5][5] = {
+	{ 1.96 , -6.49, -0.47, -7.20, -0.65},
+	{ -6.49,  3.80, -6.39,  1.50, -6.34},
+	{ -0.47, -6.39,  4.17, -1.51,  2.67},
+	{ -7.20,  1.50, -1.51,  5.70,  1.80},
+	{ -0.65, -6.34,  2.67,  1.80, -7.10}
+	};
+	//cv::Mat E = Mat(5,1,CV_64F,Scalar(0));
+	//cv::Mat V = Mat(5,5,CV_64F,Scalar(0));
+	cv::Mat E, V;
+	cv::Mat M = Mat(5,5,CV_64FC1,b);
+	//cv::eigen(M,E,V);
+	cv::eigen(M,true,E,V);
+	//cv::eigen(M,E,V,-100000,100000);
+	//cvEigenVV(M, V, E, DBL_EPSILON, -1, -1);
+	cout << "\nTEST EIGENVALUES:\n";
+	for(int i = 0; i < E.cols; i++) {
+		for(int j = 0; j < E.cols; j++) {
+			std::cout << E.at<double>(i,j) << " \t";
+		}
+		cout << "\n";
+	}
+	cout << "\nTEST EIGENVECTORS:\n";
+	for(int i = 0; i < V.cols; i++) {
+		for(int j = 0; j < V.cols; j++) {
+			std::cout << V.at<double>(i,j) << " \t";
+		}
+		cout << "\n";
+	}
+
+
 	// 3. Find eigenvectors and eigenvalues of covariance matrix.
 	// UNIT eigenvectors = principal directions.
 	// Projections of image vector x to principal directions = principal components of x.
-	cout << "\nA";
+	cout << "\n";
+//	Mat eigenvalues = ;
+//	Mat eigenvectors = ;
 	Mat eigenvalues, eigenvectors;
-	cout << "\nB";
+	cout << "\nRunning 'eigen' function... ";
 	eigen(covarianceMat, eigenvalues, eigenvectors);
-	cout << "\nC";
+	cout << "done.\n\n";
 
 	// Print eigenvalues, which are sorted descending
-	for(int i = 0; i < eigenvalues.cols; i++) std::cout << eigenvalues.at<double>(0,i) << " \t";
-
-	/*
-	CvScalar scalar;
-	CvMat* eigenvec  = cvCreateMat(pixelsPerImage, pixelsPerImage, CV_32F); //eigenvectors
-	CvMat* eigenval  = cvCreateMat(1, pixelsPerImage, CV_32F);  //eigenvalues (1xN)
-	cvZero(eigenvec);
-	cvZero(eigenval);
-	cvEigenVV(&covarianceMat, eigenvec, eigenval, 1);
-	for(int j = 0; j < eigenval->cols; j++){
-		// Access the obtained eigenvalues
-		scalar = cvGet2D(eigenval, 0, j);
-		cout << "EIG " << scalar.val[0] << "\n";
+	cout << "\nOUTPUT EIGENVALUES:\n";
+	for(int i = 0; i < eigenvalues.cols; i++) {
+		for(int j = 0; j < eigenvalues.cols; j++) {
+			std::cout << eigenvalues.at<double>(i,j) << " \t";
+		}
+		cout << "\n";
 	}
-	cvReleaseMat(&eigenvec); //house-cleaning :P 
-	cvReleaseMat(&eigenval);
-	*/
-
-
+	cout << "\nOUTPUT EIGENVECTORS:\n";
+	for(int i = 0; i < eigenvectors.cols; i++) {
+		for(int j = 0; j < eigenvectors.cols; j++) {
+			std::cout << eigenvectors.at<double>(i,j) << " \t";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
 
 	// 2. Calculate eigenface vectors from training set.
 	// 3. Keep only the M eigenvectors corresponding to the highest eigenvalues.
